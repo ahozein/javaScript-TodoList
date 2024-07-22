389f42a99d06c562ee8411bb9b839729b611491c
Amir Hossein Darroudi <aho3ein_d79@yahoo.com>
2024-07-22 14:23:40 +0330

    edit item script

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const inputInvalid = document.getElementById('input-invalid');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('items-clear');
 const searchInput = document.getElementById('filter');
-
-
+const editBtn = itemForm.querySelector('button');
+let isEditMode = false;
 
 function checkUI() {
     const items = itemList.querySelectorAll('li');
@@ -50,13 +50,36 @@ function addItemToStorage(item) {
     localStorage.setItem('items', JSON.stringify(storageItems));
 }
 
-function removeItemFromStorage(item){
+function removeItemFromStorage(item) {
     let storageItems = getItemsFromStorage();
 
     storageItems = storageItems.filter((content) => content !== item);
     localStorage.setItem('items', JSON.stringify(storageItems));
 }
 
+function removeItem(item) {
+    item.remove();
+    removeItemFromStorage(item.textContent);
+    checkUI();
+}
+
+function selectItemToEdit(selectedItem) {
+    isEditMode = true;
+
+    itemList.querySelectorAll('li').forEach(item => item.classList.remove('edit-mode'));
+    selectedItem.classList.add('edit-mode');
+
+    itemInput.value = selectedItem.textContent;
+
+    editBtn.style.backgroundColor = 'rgb(28, 74, 175)';
+    editBtn.innerHTML = '<i class="fa fa-pen"> </i> Update item';
+}
+
+function checkIfItemExists(item) {
+    const itemsFromStorge = getItemsFromStorage();
+
+    return itemsFromStorge.includes(item);
+}
 
 
 itemForm.addEventListener('submit', (e) => {
@@ -70,6 +93,24 @@ itemForm.addEventListener('submit', (e) => {
         inputInvalid.innerText = '';
     }
 
+    if (isEditMode) {
+        const selectedItem = itemList.querySelector('.edit-mode');
+
+        removeItemFromStorage(selectedItem.textContent);
+        selectedItem.remove();
+        editBtn.innerHTML = "<i class='fa fa-plus'></i> Add Item";
+        editBtn.style.backgroundColor = 'rgb(56, 56, 56)';
+
+        isEditMode = false;
+    } else {
+        if (checkIfItemExists(newItem)) {
+            inputInvalid.innerText = 'This item already exists!';
+            return;
+        } else {
+            inputInvalid.innerText = '';
+        }
+    }
+
     createItem(newItem);
     addItemToStorage(newItem);
 
@@ -80,11 +121,10 @@ itemForm.addEventListener('submit', (e) => {
 
 itemList.addEventListener('click', (e) => {
     if (e.target.id === 'remove-item' && e.target.classList.contains('fa-x')) {
-        e.target.parentElement.remove();
+        removeItem(e.target.parentElement);
+    } else {
+        selectItemToEdit(e.target);
     }
-    removeItemFromStorage(e.target.parentElement.textContent);
-
-    checkUI();
 });
 
 clearBtn.addEventListener('click', () => {
